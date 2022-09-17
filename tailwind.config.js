21db3a961234c3887ae19d62c22e5a7a7b7f18d5
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-17 11:49:30 +0200

    Add some more Tailwind min-widths
    And use with the sort dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,11 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
   ],
+  theme: {
+    minWidth: {
+      '72': '18rem', /* 288px */
+      '80': '20rem', /* 320px */
+      '96': '24rem', /* 384px */
+    }
+  }
 }
