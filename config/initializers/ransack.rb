b9938628a4cdb65eed9ee0272f78d11546c3be1a
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-17 11:52:39 +0200

    Add basic filtering
    Can only filter by one attribute at a time

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,5 +1,11 @@
 module Ransack
   class Search
+    def condition_attributes
+      groupings_attr_names = groupings.flat_map { |g| g.conditions.flat_map { |c| c.attributes.map(&:attr_name) } }
+      conditions_attr_names = conditions.flat_map { |c| c.attributes.map(&:attr_name) }
+      (groupings_attr_names + conditions_attr_names).uniq
+    end
+
     def sort_attributes
       sorts.map(&:attr_name).uniq
     end
