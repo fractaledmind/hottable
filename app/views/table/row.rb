6ce0341496afa9b18dd47917a355985cac6e36d8
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-17 14:57:52 +0200

    Add the checkbox column to our table

--- app/views/table/row.rb
+++ app/views/table/row.rb
@@ -7,7 +7,8 @@ module Views
       end
 
       def template
-        tr class: "divide-x divide-gray-200" do
+        tr class: "divide-x divide-gray-200 row-group hover:bg-gray-100" do
+          select_cell
           attributes.each do |attribute|
             render Column.new(@record, attribute:, search: @search)
           end
@@ -18,6 +19,23 @@ module Views
 
       def attributes
         @search.field_attributes
+      end
+      
+      def select_cell
+        td class: "text-center relative" do
+          input type: "checkbox",
+                id: select_identifier,
+                class: "hidden [.row-group:hover_&]:inline-block checked:inline-block peer rounded border-gray-300 text-indigo-600 focus:ring-indigo-500",
+                aria: {
+                  labelledby: "row_1 column_select"
+                }
+          span @record.id.to_s, class: "text-gray-600 inline-block [.row-group:hover_&]:hidden peer-checked:hidden"
+          label for: select_identifier, class: "absolute inset-0"
+        end
+      end
+      
+      def select_identifier
+        dom_id(@record, "select")
       end
     end
   end
