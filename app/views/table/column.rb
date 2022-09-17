17e433501c12b352c74fa67b73ac35db2cf49052
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-17 16:13:12 +0200

    Show the column grouped by

--- app/views/table/column.rb
+++ app/views/table/column.rb
@@ -10,14 +10,16 @@ module Views
       def template
         td @record.public_send(@attribute).to_s,
           **classes("px-2 py-2 text-sm text-gray-500",
-            filtered?: "bg-green-200",
-            sorted?: "bg-orange-200")
+              filtered?: "bg-green-200",
+              sorted?: "bg-orange-200",
+              grouped?: "bg-purple-200")
       end
 
       private
 
       def filtered? = @search.condition_attributes.include? @attribute
       def sorted? = @search.sort_attributes.include? @attribute
+      def grouped? = @search.batch_attribute == @attribute
     end
   end
 end
