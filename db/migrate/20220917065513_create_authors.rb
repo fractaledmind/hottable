a57773f5f36e63103ccecb6fb83b73b25c94efac
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-17 09:03:10 +0200

    Add unique index for Author
    to aid upserts

--- db/migrate/20220917065513_create_authors.rb
+++ db/migrate/20220917065513_create_authors.rb
@@ -1,7 +1,7 @@
 class CreateAuthors < ActiveRecord::Migration[7.0]
   def change
     create_table :authors do |t|
-      t.string :name
+      t.string :name, index: { unique: true }
 
       t.timestamps
     end
