92af8b334e77f23bdad060af74e59cff76f0e144
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-17 09:45:01 +0200

    Cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/index'
+  resources :books, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "books#index"
 end
