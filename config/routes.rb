008562d5aa74f631cc9c72fcc3b7b1d026aa565a
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-17 10:33:14 +0200

    Add Ransack search route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :books, only: [:index]
+  resources :books, only: [:index] do
+    collection do
+      match 'search' => 'books#search', via: [:get, :post], as: :search
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
