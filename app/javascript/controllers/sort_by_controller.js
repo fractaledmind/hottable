91329d8d1c60bf84b4ff53423475f6917540a748
Marco Roth <marco.roth@intergga.ch>
2022-09-17 22:29:01 +0100

    Add ability to sort by multiple columns

--- app/javascript/controllers/sort_by_controller.js
+++ app/javascript/controllers/sort_by_controller.js
@@ -1,5 +1,51 @@
 import ApplicationController from "./application_controller"
 
 export default class extends ApplicationController {
+  static targets = ["items", "field"]
 
+  connect() {
+    if (this.fieldTargets.length === 0) {
+      this.appendChild()
+    }
+  }
+
+  add(event) {
+    event.preventDefault()
+
+    this.appendChild()
+  }
+
+  remove(event) {
+    event.preventDefault()
+
+    event.target.closest(".fields").remove()
+    this.updateIndexes()
+  }
+
+  appendChild() {
+    const fields = this.fieldTarget.cloneNode(true)
+    fields.querySelectorAll("select").forEach(field => field.value = null)
+
+    this.itemsTarget.appendChild(fields)
+    this.updateIndexes()
+  }
+
+  updateIndexes() {
+    this.fieldTargets.forEach(field => {
+      const [column, direction] = field.querySelectorAll("select")
+
+      column.name = `q[s][${this.fieldTargets.indexOf(field)}][name]`
+      column.id = `q_s_[${this.fieldTargets.indexOf(field)}]_name`
+
+      direction.name = `q[s][${this.fieldTargets.indexOf(field)}][dir]`
+      direction.id = `q_s_[${this.fieldTargets.indexOf(field)}]_dir`
+    })
+  }
+
+  clear(event) {
+    event.preventDefault()
+
+    this.element.querySelectorAll("select").forEach(select => select.value = null)
+    this.element.closest("form").requestSubmit()
+  }
 }
--- app/views/button.rb
+++ app/views/button.rb
@@ -1,13 +1,14 @@
 module Views
   class Button < Base
-    def initialize(text, primary: false)
+    def initialize(text, primary: false, **attributes)
       @text = text
       @primary = primary
+      @attributes = attributes
     end
 
     def template
       button @text, **classes("inline-flex items-center rounded-md border border-transparent bg-gray-200 px-2.5 py-1.5 text-base font-medium text-gray-900 hover:bg-gray-300",
-        primary?: "bg-blue-500 hover:bg-blue-400 text-white")
+        primary?: "bg-blue-500 hover:bg-blue-400 text-white"), **@attributes
     end
 
     def primary? = !!@primary
