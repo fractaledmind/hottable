d52cbab8c9c32745c7b345acd0a881a0e24995e8
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-17 10:33:49 +0200

    Add Book#search controller action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,13 @@
 class BooksController < ApplicationController
+  # GET /users
   def index
     @search = Book.ransack(params[:q])
     @pagy, @records = pagy(@search.result)
+  end
+
+  # GET|POST /users/search
+  def search
+    index
+    redirect_to books_path(params.to_unsafe_hash)
   end
 end
