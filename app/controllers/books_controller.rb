7cf2dc4b1b7d11da27524a7498fde11a22fdce52
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-17 10:51:49 +0200

    Add pagination data to table footer

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,12 @@ class BooksController < ApplicationController
   # GET /users
   def index
     @search = Book.ransack(params[:q])
-    @pagy, @records = pagy(@search.result)
+    @pagy, @records = pagy(@search.result, items: params.fetch(:page_items, 20))
   end
 
   # GET|POST /users/search
   def search
     index
-    redirect_to books_path(params.to_unsafe_hash)
+    redirect_to books_path(params.to_unsafe_hash.except(:authenticity_token, :action, :controller, :commit))
   end
 end
