82ecd60a6dc6c327d652f0b23eeff4472ad96e28
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-17 09:17:32 +0200

    [GENERATE]  bundle exec rails db:migrate
    == 20220917065513 CreateAuthors: migrating ====================================
    -- create_table(:authors)
       -> 0.0072s
    == 20220917065513 CreateAuthors: migrated (0.0073s) ===========================
    
    == 20220917071354 CreateBooks: migrating ======================================
    -- create_table(:books)
       -> 0.0067s
    == 20220917071354 CreateBooks: migrated (0.0067s) =============================
    
    == 20220917071558 CreateBookAuthors: migrating ================================
    -- create_table(:book_authors)
       -> 0.0083s
    == 20220917071558 CreateBookAuthors: migrated (0.0084s) =======================

---
+++ db/schema.rb
@@ -0,0 +1,51 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_09_17_071558) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_authors_on_name", unique: true
+  end
+
+  create_table "book_authors", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_book_authors_on_author_id"
+    t.index ["book_id"], name: "index_book_authors_on_book_id"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.decimal "average_rating", precision: 3, scale: 2
+    t.string "isbn"
+    t.string "isbn13"
+    t.string "language_code"
+    t.integer "num_pages"
+    t.integer "ratings_count"
+    t.integer "text_reviews_count"
+    t.date "published_on"
+    t.string "publisher"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["isbn"], name: "index_books_on_isbn", unique: true
+  end
+
+  add_foreign_key "book_authors", "authors"
+  add_foreign_key "book_authors", "books"
+end
