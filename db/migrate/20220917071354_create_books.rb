502694688c6de557e21ccedc0a8d6fac314c8645
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-17 09:14:48 +0200

    [GENERATE] bundle exec rails g model Book title:string average_rating:decimal{3-2} isbn:string isbn13:string language_code:string num_pages:integer ratings_count:integer text_reviews_count:integer published_on:date publisher:string
    invoke  active_record
    create    db/migrate/20220917065652_create_books.rb
    create    app/models/book.rb

---
+++ db/migrate/20220917071354_create_books.rb
@@ -0,0 +1,18 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.decimal :average_rating, precision: 3, scale: 2
+      t.string :isbn
+      t.string :isbn13
+      t.string :language_code
+      t.integer :num_pages
+      t.integer :ratings_count
+      t.integer :text_reviews_count
+      t.date :published_on
+      t.string :publisher
+
+      t.timestamps
+    end
+  end
+end
