50ce6b7f2dd359a3769a3368de20b7cce405e947
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-17 13:51:01 +0200

    Fix mistake with Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,12 @@ module.exports = {
     require('@tailwindcss/forms'),
   ],
   theme: {
-    minWidth: {
-      '72': '18rem', /* 288px */
-      '80': '20rem', /* 320px */
-      '96': '24rem', /* 384px */
+    extend: {
+      minWidth: {
+        '72': '18rem', /* 288px */
+        '80': '20rem', /* 320px */
+        '96': '24rem', /* 384px */
+      }
     }
   }
 }
