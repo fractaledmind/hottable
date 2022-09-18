f8c93a7d9f15e290726b7a377d4d7d7e156ed093
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-18 02:32:08 +0200

    Add a schema for the column types with icons in the table header

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,4 +11,21 @@ class Book < ApplicationRecord
   def self.ransackable_attributes(_auth_object = nil)
     ransortable_attributes + _ransackers.keys
   end
+  
+  def self.attribute_schema
+    {
+      title: :text,
+      average_rating: :numeric,
+      isbn: :string,
+      isbn13: :string,
+      language_code: :enum,
+      num_pages: :numeric,
+      ratings_count: :numeric,
+      text_reviews_count: :numeric,
+      published_on: :date,
+      publisher: :string,
+      created_at: :datetime,
+      updated_at: :datetime
+    }
+  end
 end
--- app/views/table/header.rb
+++ app/views/table/header.rb
@@ -7,17 +7,31 @@ module Views
       end
 
       def template
-        th Book.human_attribute_name(@attribute),
-           scope: :col,
-           **classes("sticky top-0 z-10 bg-opacity-75 backdrop-blur backdrop-filter whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900",
+        th scope: :col,
+           **classes("sticky top-0 z-10 bg-gray-50 border-b whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900 space-x-1",
            filtered?: "bg-green-300",
-           sorted?: "bg-orange-300")
+           sorted?: "bg-orange-300") do
+          render Bootstrap::Icon.new(attribute_icon), aria: { hidden: "true" }
+          span Book.human_attribute_name(@attribute)
+        end
       end
 
       private
 
       def filtered? = @search.condition_attributes.include? @attribute
       def sorted? = @search.sort_attributes.include? @attribute
+      
+      def attribute_icon
+        type = Book.attribute_schema.fetch(@attribute.to_sym)
+        {
+          string: "type",
+          text: "text-paragraph",
+          date: "calendar",
+          numeric: "hash",
+          enum: "circle-fill",
+          datetime: "clock-fill"
+        }.fetch(type)
+      end
     end
   end
 end
