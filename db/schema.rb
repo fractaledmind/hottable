cccd355aa0c81db1212ed646cef6f27860bb1f38
Joel Drapper <joel@drapper.me>
2022-09-18 23:03:39 +0100

    Create views migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_17_071558) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_18_220202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_071558) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["isbn"], name: "index_books_on_isbn", unique: true
+  end
+
+  create_table "views", force: :cascade do |t|
+    t.string "name"
+    t.string "parameters"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "book_authors", "authors"
