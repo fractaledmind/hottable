901d65690f975800abf37c994b6f299b69df7735
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-20 15:13:24 +0200

    Rename the details-dropdown Stimulus controller to details-popover
    And keep aria-expanded in sync with open

--- app/javascript/controllers/details_popover_controller.js
+++ app/javascript/controllers/details_popover_controller.js
@@ -3,6 +3,12 @@ import ApplicationController from "./application_controller"
 export default class extends ApplicationController {
   static targets = ['button']
 
+  connect() {
+    this.element.addEventListener("toggle", (event) => {
+      this.buttonTarget.setAttribute('aria-expanded', event.currentTarget.open)
+    });
+  }
+
   hide(event) {
     if (event && (this.element.contains(event.target))) {
       return;
