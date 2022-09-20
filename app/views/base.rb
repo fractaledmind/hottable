c1279f6df720e7a970f5ec5feb99a540ddefd561
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-20 15:14:53 +0200

    Remove unneeded patch from base Phlex components

--- app/views/base.rb
+++ app/views/base.rb
@@ -1,9 +1,5 @@
 require "phlex/rails"
 
-class Phlex::Component
-  alias_method :safe_expr_append=, :raw
-end
-
 module Views
   class Base < Phlex::Component
     include ActionView::Helpers::AssetUrlHelper
