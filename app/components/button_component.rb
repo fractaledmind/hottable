79b9bfbd0455c62e536be8af89e6a871ab9a5e1e
Stephen Margheim <stephen.margheim@gmail.com>
2022-09-27 14:28:22 +0200

    Make Button component have less aggressive default styles

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -1,23 +1,23 @@
 class ButtonComponent < ApplicationComponent
   class Struct < ApplicationComponent::Struct
-    def initialize(primary: false)
-      @primary = primary
+    def initialize(type: nil, primary: false)
+      @type = type || (:primary if primary)
     end
-    
+
     def base
       {
         class: tokens(
           "cursor-pointer inline-flex items-center rounded-md border border-transparent px-2.5 py-1.5 text-base font-medium gap-2",
           primary?: "bg-blue-500 hover:bg-blue-400 text-white",
-          default?: "bg-gray-200 hover:bg-gray-300 text-gray-900"
+          secondary?: "bg-gray-200 hover:bg-gray-300 text-gray-900",
         ),
       }
     end
-    
+
     private
-    
-    def primary? = !!@primary
-    def default? = !primary?
+
+    def primary? = @type == :primary
+    def secondary? = @type == :secondary
   end
 
   def self.struct(*args, **kwargs)
